e6283977eeed6758d111c1d0bc7c40090769df14
kevinten10 <596823919@qq.com>
2018-06-29 14:05:07 +0800

    --version2.0   --2018.6.28   --使用观察者模式统计用户登录数量

---
+++ src/main/java/com/wang/observer/UserLoginObserver.java
@@ -0,0 +1,15 @@
+package com.wang.observer;
+
+import java.util.Observer;
+
+/**
+ * 观察者 - 观察者
+ * <p>
+ * 统计User在线人数
+ *
+ * @date 2018/6/28
+ * @auther ten
+ */
+public interface UserLoginObserver extends Observer {
+
+}
